222e97bc100e4d2b1242e3d3dd9543f8733df01f
Sahilkaramkar <126070964+14-sahil@users.noreply.github.com>
2025-11-09 22:38:36 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
   productId: { type: String, required: true, unique: true },
-  name: String,
+  name: String, 
   price: Number,
   description: String,
   image: String
